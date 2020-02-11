5260478fba79a30c9078c7bc04ec4f9f4ac15d01
Kang.Yuchan <ikimaku0517@gmail.com>
2020-02-11 16:28:20 +0900

    Connect MongoDB Atlas with mongoose

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import mongoose from 'mongoose';
 dotenv.config();
+
+//app
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//DB
+mongoose
+	.connect(process.env.DATABASE, {
+		useNewUrlParser: true,
+		useCreateIndex: true
+	})
+	.then(() => console.log('DB Connected!'));
 
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 const handleGet = (req, res) => res.send('hello from node');
 
+//routes
 app.get('/', handleGet);
 app.listen(PORT, handleListening);
